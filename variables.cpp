2987b05aebe1996235f2360bd9e09a1d370405dd
Alexandre Adonai <alexandre.adonai2002@hotmail.com>
2022-05-02 18:00:50 -0300

    Starting variables

---
+++ variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "a" << endl;
+    cout << "b" << endl;
+    cout << "c" << endl;
+    cout << "d" << endl;
+
+    return 0;
+    
+}
